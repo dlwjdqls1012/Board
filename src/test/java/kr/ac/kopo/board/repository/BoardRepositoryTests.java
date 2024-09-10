ff4f92351fb619426db131b5cbc4a77c9b7f3b65
dlwjdqls1012 <dlwjdqls1356@gmail.com>
2024-09-10 16:33:04 +0900

    20240910 마무리

--- src/test/java/kr/ac/kopo/board/repository/BoardRepositoryTests.java
+++ src/test/java/kr/ac/kopo/board/repository/BoardRepositoryTests.java
@@ -5,6 +5,10 @@ import kr.ac.kopo.board.entity.Member;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Arrays;
@@ -59,5 +63,23 @@ public class BoardRepositoryTests {
         for (Object[] arr: result){
             System.out.println(Arrays.toString(arr));
         }
+    }
+
+    @Test
+    public void testBoardWithReplyCount(){
+        Pageable pageable = (Pageable) PageRequest.of(0, 19, Sort.by("bno").descending());
+        Page<Object[]> result = boardRepository.getBoardWithReplyCount(pageable);
+
+        result.get().forEach(row ->{
+            Object[] arr = (Object[]) row;
+            System.out.println(Arrays.toString(arr));
+        });
+    }
+
+    @Test
+    public void restRead3(){
+        Object result = boardRepository.getBoardByBno(99L);
+        Object[] arr = (Object[]) result;
+        System.out.println(Arrays.toString(arr));
     }
 }
